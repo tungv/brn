cda8cb0aec15eb66de8d961707031814e7e61e9d
Tung Vu <me@tungv.com>
2021-06-27 02:20:17 +0700

    remove Truthy because you can never DO IT

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 type Tuple = any[];
 type Falsy = 0 | 0n | null | false | "" | undefined | void;
-type Truthy = Extract<object | symbol | Function | true | number | string | bigint, Falsy>;
-type TestFn<InputArgs extends Tuple, Outcome extends Falsy | Truthy> = (...args: InputArgs) => Outcome;
+type TestFn<InputArgs extends Tuple, Outcome extends any> = (...args: InputArgs) => Outcome;
 type BranchFn<InputArgs extends Tuple, Output> = (...args: InputArgs) => Output;
 
 interface BranchModule {
@@ -10,12 +9,6 @@ interface BranchModule {
     left: BranchFn<InputArgs, LeftOutput>,
     right: BranchFn<InputArgs, RightOutput>
   ): (...args: InputArgs) => RightOutput;
-
-  <InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
-    test: TestFn<InputArgs, Truthy>,
-    left: BranchFn<InputArgs, LeftOutput>,
-    right: BranchFn<InputArgs, RightOutput>
-  ): (...args: InputArgs) => LeftOutput;
 
   <InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
     test: TestFn<InputArgs, any>,
@@ -27,11 +20,6 @@ interface BranchModule {
     test: TestFn<InputArgs, Falsy>,
     left: BranchFn<InputArgs, LeftOutput>
   ): (...args: InputArgs) => InputArgs[0];
-
-  <InputArgs extends Tuple, LeftOutput = InputArgs[0]>(
-    test: TestFn<InputArgs, Truthy>,
-    left: BranchFn<InputArgs, LeftOutput>
-  ): (...args: InputArgs) => LeftOutput;
 
   <InputArgs extends Tuple, LeftOutput = InputArgs[0]>(
     test: TestFn<InputArgs, any>,
