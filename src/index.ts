9a74e14cc4adbc9eb9862d47ae342a5abbc51e36
Tung Vu <me@tungv.com>
2021-06-27 01:38:50 +0700

    support naked export as well as esm default export

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,54 @@
 type Tuple = any[];
-type Falsy = 0 | 0n | null | false | '' | undefined | void;
+type Falsy = 0 | 0n | null | false | "" | undefined | void;
 type Truthy = Extract<object | symbol | Function | true | number | string | bigint, Falsy>;
-type TestFn<InputArgs extends Tuple, Outcome extends Falsy | Truthy > = (...args: InputArgs) => Outcome;
+type TestFn<InputArgs extends Tuple, Outcome extends Falsy | Truthy> = (...args: InputArgs) => Outcome;
 type BranchFn<InputArgs extends Tuple, Output> = (...args: InputArgs) => Output;
 
+interface BranchModule {
+  <InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
+    test: TestFn<InputArgs, Falsy>,
+    left: BranchFn<InputArgs, LeftOutput>,
+    right: BranchFn<InputArgs, RightOutput>
+  ): (...args: InputArgs) => RightOutput;
 
-export default function branch<InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
-  test: TestFn<InputArgs, Falsy>,
-  left: BranchFn<InputArgs, LeftOutput>,
-  right: BranchFn<InputArgs, RightOutput>
-): (...args: InputArgs) => RightOutput;
+  <InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
+    test: TestFn<InputArgs, Truthy>,
+    left: BranchFn<InputArgs, LeftOutput>,
+    right: BranchFn<InputArgs, RightOutput>
+  ): (...args: InputArgs) => LeftOutput;
 
-export default function branch<InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
-  test: TestFn<InputArgs, Truthy>,
-  left: BranchFn<InputArgs, LeftOutput>,
-  right: BranchFn<InputArgs, RightOutput>
-): (...args: InputArgs) => LeftOutput;
+  <InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
+    test: TestFn<InputArgs, any>,
+    left: BranchFn<InputArgs, LeftOutput>,
+    right: BranchFn<InputArgs, RightOutput>
+  ): (...args: InputArgs) => LeftOutput | RightOutput;
 
-export default function branch<InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
-  test: TestFn<InputArgs, any>,
-  left: BranchFn<InputArgs, LeftOutput>,
-  right: BranchFn<InputArgs, RightOutput>
-): (...args: InputArgs) => LeftOutput | RightOutput;
+  <InputArgs extends Tuple, LeftOutput = InputArgs[0]>(
+    test: TestFn<InputArgs, Falsy>,
+    left: BranchFn<InputArgs, LeftOutput>
+  ): (...args: InputArgs) => InputArgs[0];
 
-export default function branch<InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
-  test: TestFn<InputArgs, Falsy>,
-  left: BranchFn<InputArgs, LeftOutput>,
-): (...args: InputArgs) => InputArgs[0];
+  <InputArgs extends Tuple, LeftOutput = InputArgs[0]>(
+    test: TestFn<InputArgs, Truthy>,
+    left: BranchFn<InputArgs, LeftOutput>
+  ): (...args: InputArgs) => LeftOutput;
 
-export default function branch<InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
-  test: TestFn<InputArgs, Truthy>,
-  left: BranchFn<InputArgs, LeftOutput>,
-): (...args: InputArgs) => LeftOutput;
+  <InputArgs extends Tuple, LeftOutput = InputArgs[0]>(
+    test: TestFn<InputArgs, any>,
+    left: BranchFn<InputArgs, LeftOutput>
+  ): (...args: InputArgs) => LeftOutput | InputArgs[0];
 
-export default function branch<InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
-  test: TestFn<InputArgs, any>,
-  left: BranchFn<InputArgs, LeftOutput>,
-): (...args: InputArgs) => LeftOutput | InputArgs[0];
+  <InputArgs extends Tuple>(test: TestFn<InputArgs, any>): (...args: InputArgs) => InputArgs[0];
+}
 
-
-export default function branch<InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
-  test: TestFn<InputArgs, any>,
-): (...args: InputArgs) => InputArgs[0];
-
-export default function branch<InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
+const branch: BranchModule = function <InputArgs extends Tuple, LeftOutput = InputArgs[0], RightOutput = InputArgs[0]>(
   test: TestFn<InputArgs, any>,
   left?: BranchFn<InputArgs, LeftOutput>,
   right?: BranchFn<InputArgs, RightOutput>
 ) {
   let leftActual = left ?? identity;
   let rightActual = right ?? identity;
-  return function(...args: InputArgs) {
+  return function (...args: InputArgs) {
     if (test(...args)) {
       return leftActual(...args);
     }
@@ -59,8 +57,11 @@ export default function branch<InputArgs extends Tuple, LeftOutput = InputArgs[0
   };
 };
 
+export default branch;
+module.exports = branch;
+
 interface IdentityFn<InputArgs extends Tuple> {
-  (...args: InputArgs): InputArgs[0]
+  (...args: InputArgs): InputArgs[0];
 }
 
-const identity:IdentityFn<any> = (x) => x
+const identity: IdentityFn<any> = (x) => x;
